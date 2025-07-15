c9fbfb385509423d265b40645b3f3e514a292baa
lucasmajorana <lucasmajorana@hotmail.com>
2025-07-14 22:47:52 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
+import logo from '/src/assets/logo.png';
+import texto from '/src/assets/texto.png';
+
 function Header() {
   return (
     <header>
       <h1>
-        <img src="/assets/logo.png" alt="Logo La Pausa" />
-        <img src="/assets/texto.png" alt="Texto La Pausa" />
+        <img src={logo} alt="Logo La Pausa" />
+        <img src={texto} alt="Texto La Pausa" />
         <span className="sr-only">La Pausa - Posadas</span>
       </h1>
     </header>
-  )
+  );
 }
+
 export default Header
