f928cdc0b13c8e7ae31c04ce200cd07d6d42daa3
lucasmajorana <lucasmajorana@hotmail.com>
2025-07-14 23:36:04 -0300

    Update Contacto.jsx

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,9 +1,10 @@
 function Contacto() {
   return (
     <main className="contacto">
-      <h2 className="titulos">Contacto</h2>
-
       <section className="contacto-contenido">
+        <div>
+          <h2>Contacto</h2>
+        </div>
         <div className="columna">
           <address>
             Misiones 5357<br />
@@ -31,7 +32,7 @@ function Contacto() {
       </section>
 
       <section className="mapa">
-        <h3 className="titulos">¿Dónde estamos?</h3>
+        <h3>¿Dónde estamos?</h3>
         <iframe
           title="Ubicación en Google Maps"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3537.5157747990553!2d-55.895808224577896!3d-27.39829327644003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9457befdbae1b369%3A0xcc47f9491f70c62f!2sMisiones%205357%2C%20Posadas%2C%20Misiones!5e0!3m2!1ses-419!2sar!4v1718918213342!5m2!1ses-419!2sar"
